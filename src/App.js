201f5256567459f18e18a72ec657c3292212cc2a
IvaTsu <ivan.tsud@gmail.com>
2020-03-10 22:37:21 +0200

    WIP: download the pdf, no errors

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
         <h3>Hello, this is a H3 tag</h3>
         <p className="para">a pararaph</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <img id="doNotInclude" src={logo} alt="react"></img>
-        <img id="doNotInclude" src={logo2} alt="react"></img>
+        <img id="doNotInclude" src={""} alt="react"></img>
+        {/* <img id="doNotInclude" src={logo2} alt="react"></img> */}
         <p id="doNotInclude">sfdjhjhgkjdfhgkj</p>
       </DownloadPDF>
     </div>
