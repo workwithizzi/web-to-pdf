1bbb2dc3b5d7a53448e7df47c5c28292b80cd789
IvaTsu <ivan.tsud@gmail.com>
2020-03-10 22:23:07 +0200

    WIP: generate PDF

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import DownloadPDF from './DownloadPDF';
+
 import './App.css';
+import logo from './logo.svg';
+import logo2 from './logo512.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DownloadPDF>
+        <h3>Hello, this is a H3 tag</h3>
+        <p className="para">a pararaph</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <img id="doNotInclude" src={logo} alt="react"></img>
+        <img id="doNotInclude" src={logo2} alt="react"></img>
+        <p id="doNotInclude">sfdjhjhgkjdfhgkj</p>
+      </DownloadPDF>
     </div>
   );
 }
